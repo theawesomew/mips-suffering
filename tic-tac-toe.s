68fc84b0dc0482ed06d1f96468b493b20e0882c5
Will Kennedy <will.kennedy@wisetechglobal.com>
2023-02-23 21:36:00 +1100

    Evaluation function is working!

--- tic-tac-toe.s
+++ tic-tac-toe.s
@@ -4,10 +4,19 @@ main:
 	la $a0, state
 
 	push $ra
-
 	jal render_board
-
 	pop $ra
+
+    li $a0, 2
+    la $a1, state
+
+    push $ra
+    jal has_won
+    pop $ra
+
+    move $a0, $v0
+    li $v0, 1
+    syscall
 
 	li $v0, 0
 	jr $ra
@@ -115,6 +124,84 @@ render_piece__nought:
 render_piece__epilogue:
 	jr $ra
 
+has_won:
+# $a0 -> the player we're checking if they have won
+# $a1 -> the current board state
+
+# 001010100 -> 84
+# 100010001 -> 273 
+# 100100100 -> 293
+# 010010010 -> 146
+# 001001001 -> 73 
+# 111000000 -> 448
+# 000111000 -> 56
+# 000000111 -> 7
+
+has_won__prologue:
+    # $t0 -> the current index
+    # $t1 -> the bitstate of the board
+    li $t0, 0
+    li $t1, 0
+    li $t3, 1
+
+has_won__body:
+    
+has_won__create_bitstate:
+    beq $t0, 9, has_won__evaluate
+
+    push $a1
+
+    add $a1, $a1, $t0
+    lb $a1, 0($a1)
+
+    beq $a1, $a0, has_won__add_value_to_bitstate 
+
+    pop $a1
+
+    add $t0, $t0, 1
+
+    j has_won__create_bitstate
+
+has_won__add_value_to_bitstate:
+    sllv $t4, $t3, $t0
+    add $t1, $t1, $t4
+    add $t0, $t0, 1
+    pop $a1
+    j has_won__create_bitstate
+
+has_won__evaluate:
+    andi $t4, $t1, 7
+    beq $t4, 7, has_won__true
+
+    andi $t4, $t1, 56
+    beq $t4, 56, has_won__true
+
+    andi $t4, $t1, 73
+    beq $t4, 73, has_won__true
+
+    andi $t4, $t1, 84
+    beq $t4, 84, has_won__true
+
+    andi $t4, $t1, 146
+    beq $t4, 146, has_won__true 
+
+    andi $t4, $t1, 273
+    beq $t4, 273, has_won__true
+
+    andi $t4, $t1, 293
+    beq $t4, 293, has_won__true
+
+    andi $t4, $t1, 446
+    beq $t4, 446, has_won__true    
+
+has_won__false:
+    li $v0, 0
+    jr $ra
+
+has_won__true:
+    li $v0, 1
+    jr $ra
+
 .data
-	state: .byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+	state: .byte 0, 0, 1, 2, 2, 2, 1, 0, 0
     test: .asciiz "GOT HERE!"
